490f2e571357b12e77b1680385060e899bcbabb8
jukaveka <juhokavekari@hotmail.com>
2025-11-09 15:27:52 +0200

    Determine review rating color based on rating score

--- src/components/RepositoryList/RepositoryView/ReviewRating.jsx
+++ src/components/RepositoryList/RepositoryView/ReviewRating.jsx
@@ -13,10 +13,29 @@ const styles = StyleSheet.create({
   },
 });
 
+const determineRatingLevel = (rating) => {
+  switch (true) {
+    case rating <= 100 && rating >= 80:
+      return 5;
+    case rating <= 79 && rating >= 60:
+      return 4;
+    case rating <= 59 && rating >= 40:
+      return 3;
+    case rating <= 39 && rating >= 20:
+      return 2;
+    case rating <= 19 && rating >= 0:
+      return 1;
+    default:
+      return null;
+  }
+};
+
 const ReviewRating = ({ rating }) => {
+  const ratingLevel = determineRatingLevel(rating);
+
   return (
     <View style={styles.container}>
-      <Rating ratingLevel={5}> {rating} </Rating>
+      <Rating ratingLevel={ratingLevel}> {rating} </Rating>
     </View>
   );
 };
